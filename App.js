f9a159ad3b9dd3da3a54c29fdbbb1067fdecf829
Elmar Ismayilov <bamazy@gmail.com>
2024-03-05 11:36:38 +0800

    Notification added

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator, TransitionPresets} from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  PermissionsAndroid,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+import messaging from "@react-native-firebase/messaging";
 import HomeScreen from "./screens/HomeScreen";
-import React from "react";
-import {Provider} from "react-redux";
-import store from './redux/store';
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import Article from "./screens/Article";
 import VideoArticle from "./screens/VideoArticle";
 import Login from "./screens/Login";
@@ -23,130 +33,128 @@ import Uskoro from "./screens/uskoro";
 import About from "./screens/About";
 import Settings from "./screens/Settings";
 
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
 
-
+    return unsubscribe;
+  }, []);
 
   const Stack = createStackNavigator();
 
   return (
-      <Provider store={store}>
-        <NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="HomeScreen"
+          screenOptions={{
+            ...TransitionPresets.SlideFromRightIOS, // Use the sliding animation
+          }}
+        >
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
 
-          <Stack.Navigator initialRouteName="HomeScreen"
-                           screenOptions={{
-                             ...TransitionPresets.SlideFromRightIOS, // Use the sliding animation
-                           }}
-          >
+          <Stack.Screen
+            name="Article"
+            component={Article}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="VideoArticle"
+            component={VideoArticle}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="Article"
-                component={Article}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="HayatPlay"
+            component={HayatPlay}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="VideoArticle"
-                component={VideoArticle}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="Uskoro"
+            component={Uskoro}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="HayatPlay"
-                component={HayatPlay}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="SubCategory"
+            component={SubCategory}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="Uskoro"
-                component={Uskoro}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="TvShow"
+            component={TVShow}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="SubCategory"
-                component={SubCategory}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="SubTvShow"
+            component={SubTvShow}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LiveTV"
+            component={LiveTv}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="VODcategories"
+            component={VODcategories}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Account"
+            component={Account}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="About"
+            component={About}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="TvShow"
-                component={TVShow}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false, animationEnabled: false }}
+          />
 
-            <Stack.Screen
-                name="SubTvShow"
-                component={SubTvShow}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="LiveTV"
-                component={LiveTv}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="VODcategories"
-                component={VODcategories}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="Account"
-                component={Account}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="About"
-                component={About}
-                options={{ headerShown: false }}
-            />
+          <Stack.Screen
+            name="Survey"
+            component={Survey}
+            options={{ headerShown: false, animationEnabled: false }}
+          />
 
-            <Stack.Screen
-                name="Login"
-                component={Login}
-                options={{ headerShown: false ,animationEnabled:false }}
-            />
+          <Stack.Screen
+            name="Settings"
+            component={Settings}
+            options={{ headerShown: false, animationEnabled: false }}
+          />
 
-            <Stack.Screen
-                name="Survey"
-                component={Survey}
-                options={{ headerShown: false ,animationEnabled:false }}
-            />
+          <Stack.Screen
+            name="TermsOfService"
+            component={TermsOfService}
+            options={{ headerShown: false }}
+          />
 
-            <Stack.Screen
-                name="Settings"
-                component={Settings}
-                options={{ headerShown: false ,animationEnabled:false }}
-            />
-
-
-
-            <Stack.Screen
-                name="TermsOfService"
-                component={TermsOfService}
-                options={{ headerShown: false ,}}
-            />
-
-            <Stack.Screen
-                name="Signup"
-                component={Signup}
-                options={{ headerShown: false ,animationEnabled:false }}
-            />
-
-          </Stack.Navigator>
-
-
-
-
-
-        </NavigationContainer>
-      </Provider>
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ headerShown: false, animationEnabled: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
@@ -154,8 +162,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
 
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
