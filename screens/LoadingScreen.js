dfdc13095dc02f8fbbdb7a0750aee971c0e7b9de
Elmar Ismayilov <bamazy@gmail.com>
2024-03-10 16:55:53 +0800

    fix loading indicator to center

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import {View, Text} from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  Dimensions,
+} from "react-native";
+
+let ScreenHeight = Dimensions.get("window").height;
 
 const LoadingScreen = () => {
-    return (
-        <View>
-            <Text>LOADING</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator color="#1A2F5A" />
+    </View>
+  );
 };
 
 export default LoadingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: ScreenHeight,
+  },
+});
