c8db37698c7d808aa190c94a924af2c5c7314e33
Elmar Ismayilov <bamazy@gmail.com>
2024-03-15 23:50:16 +0800

    refactor to combine components

--- components/HomeScreen/HighlightNews.js
+++ components/HomeScreen/HighlightNews.js
@@ -1,61 +1,52 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyleSheet, View } from "react-native";
 import { useSelector } from "react-redux";
 
-import Priority1 from "../Priority1";
-import Priority2 from "../Priority2";
-import Priority3 from "../Priority3";
-import Priority4 from "../Priority4";
-import Priority5 from "../Priority5";
-import image1 from "../../assets/news/newsBack-1.png";
-import image2 from "../../assets/news/newsBack-2.png";
-import image3 from "../../assets/news/newsBack-3.png";
-import image4 from "../../assets/news/newsBack-4.png";
-import image5 from "../../assets/news/newsBack-5.png";
-import image6 from "../../assets/news/newsBack-6.png";
-import image7 from "../../assets/news/newsBack-7.png";
-import article from "../../screens/Article";
-import Priority6 from "../Priority6";
+import Priority from "../Priority";
+// import Priority1 from "../Priority1";
+// import Priority2 from "../Priority2";
+// import Priority3 from "../Priority3";
+// import Priority4 from "../Priority4";
+// import Priority5 from "../Priority5";
+// import image1 from "../../assets/news/newsBack-1.png";
+// import image2 from "../../assets/news/newsBack-2.png";
+// import image3 from "../../assets/news/newsBack-3.png";
+// import image4 from "../../assets/news/newsBack-4.png";
+// import image5 from "../../assets/news/newsBack-5.png";
+// import image6 from "../../assets/news/newsBack-6.png";
+// import image7 from "../../assets/news/newsBack-7.png";
+// import article from "../../screens/Article";
+// import Priority6 from "../Priority6";
 
 const HighlightNews = () => {
   const highlightData = useSelector(
     (state) => state.selectedContent.highlightData
   );
 
-  const renderComponent = (article) => {
-    switch (article.priority) {
-      case 2:
-        return <Priority2 {...articleProps(article)} />;
-      case 3:
-        return <Priority3 {...articleProps(article)} />;
-      case 4:
-        return <Priority4 {...articleProps(article)} />;
-      case 5:
-        return <Priority6 {...articleProps(article)} />;
-      case 6:
-        return <Priority6 {...articleProps(article)} />;
-      default:
-        return null;
-    }
-  };
+  // const renderComponent = (article) => {
+  //   switch (article.priority) {
+  //     case 2:
+  //       return <Priority2 {...articleProps(article)} />;
+  //     case 3:
+  //       return <Priority3 {...articleProps(article)} />;
+  //     case 4:
+  //       return <Priority4 {...articleProps(article)} />;
+  //     case 5:
+  //       return <Priority6 {...articleProps(article)} />;
+  //     case 6:
+  //       return <Priority6 {...articleProps(article)} />;
+  //     default:
+  //       return null;
+  //   }
+  // };
 
-  const articleProps = (article) => ({
-    articleID: article._id,
-    articleTitle: article.title,
-    image: { uri: article.image_list[0]?.url }, // Format as an object with a uri field
-    articleSubtitle: article.subtitle,
-    /*
-        articleText: article.text,
-        editor:article.editor,
-        dateTime:article.date_time,
-        photoPost:article.photo_post,
-        published:article.published,
-        urlTitle: article.url_title,
-        videoPost:article.video_post,
-        imageList: article.image_list,
-
-    */
-  });
+  // const articleProps = (article) => ({
+  //   articleID: article._id,
+  //   articleTitle: article.title,
+  //   image: { uri: article.image_list[0]?.url },
+  //   articleSubtitle: article.subtitle,
+  //   priority: article.priority,
+  // });
 
   // Categorize articles by priority
   const articlesByPriority = {};
@@ -81,10 +72,9 @@ const HighlightNews = () => {
 
   return (
     <View style={styles.container}>
-      {sortedData.map((article) => {
-        // Directly render the component with a key
-        return <View key={article._id}>{renderComponent(article)}</View>;
-      })}
+      {sortedData.map((article) => (
+        <Priority key={article._id} article={article} />
+      ))}
     </View>
   );
 };
