a2f5c53850cda953b2676f3ed09af03ae0fefa06
Elmar Ismayilov <bamazy@gmail.com>
2024-03-18 17:23:15 +0800

    combine home article arrays to use with section list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,7 @@ import {
   Alert,
   Platform,
   ScrollView,
-  FlatList,
+  SectionList,
   StatusBar,
   StyleSheet,
   View,
@@ -34,6 +34,7 @@ import Najnovije from "../components/HomeScreen/Najnovije";
 import LoadingScreen from "../components/Common/LoadingScreen";
 import AdPlacement from "../components/Ads/AdPlacement";
 import NoConnection from "../components/NoConnection";
+import Priority from "../components/Priority";
 
 const theme = {
   ...DefaultTheme,
@@ -54,6 +55,7 @@ const HomeScreen = () => {
   const userInfo = useSelector((state) => state.user.userInfo);
   const [userInfoLoaded, setUserInfoLoaded] = useState(false);
   const [isConnectionError, setIsConnectionError] = useState(false);
+  const [data, setData] = useState([]);
 
   // Function to update loading state
   const expoPushToken = useSelector(
@@ -61,7 +63,47 @@ const HomeScreen = () => {
   );
 
   //console.log("Expo Push Token from Redux:", expoPushToken);
-  const { loading: isLoading } = useSelector((state) => state.selectedContent);
+  const { loading: isLoading, categoriesData } = useSelector(
+    (state) => state.selectedContent
+  );
+
+  const getCategoryName = (categoryId) => {
+    const category = categoriesData?.find((c) => c._id == categoryId);
+    return category ? category.name : "Unknown Category";
+  };
+
+  const setPriorityAccordingToSortOrder = (articles, sortOrder) => {
+    const sortedData = [];
+    articles.forEach((article, index) => {
+      const newArticle = { ...article, priority: sortOrder[index] };
+      sortedData.push(newArticle);
+    });
+    return sortedData;
+  };
+
+  const sortHighlightArticlesAccordingToPriority = (articles) => {
+    const sortOrder = [2, 5, 4, 3, 4, 3, 6];
+    const sortedData = [];
+
+    const articlesByPriority = {};
+    articles.forEach((article) => {
+      if (!articlesByPriority[article.priority]) {
+        articlesByPriority[article.priority] = [];
+      }
+      articlesByPriority[article.priority].push(article);
+    });
+
+    sortOrder.forEach((priority) => {
+      if (
+        articlesByPriority[priority] &&
+        articlesByPriority[priority].length > 0
+      ) {
+        // Take the next article from the corresponding category
+        sortedData.push(articlesByPriority[priority].shift());
+      }
+    });
+    return sortedData;
+  };
 
   //console.log(userInfo);
   useEffect(() => {
@@ -80,8 +122,39 @@ const HomeScreen = () => {
         const mainArticlesResponse = await axios.get(
           `${API_URL}/articles/main`
         );
-
         dispatch(setMainArticles(mainArticlesResponse.data)); // Dispatch the action for main articles
+
+        const categoryDataSortOrders = [
+          [2, 3, 3, 5, 5, 5, 3],
+          [2, 4, 4, 3, 3, 5, 5],
+          [2, 5, 4, 4, 3, 3, 5],
+        ];
+
+        const highlightSortedData = sortHighlightArticlesAccordingToPriority(
+          highlightResponse.data
+        );
+
+        const listData = [
+          { title: "Highlight", data: highlightSortedData, adId: 2 },
+        ];
+        mainArticlesResponse.data.forEach((categoryArticles, categoryIndex) => {
+          const categoryName = getCategoryName(categoryArticles[0].category);
+          const algoIndex = categoryIndex % 3;
+          const selectedArticles = categoryArticles.slice(0, 7);
+
+          const categorySortedData = setPriorityAccordingToSortOrder(
+            selectedArticles,
+            categoryDataSortOrders[algoIndex]
+          );
+
+          listData.push({
+            title: categoryName,
+            data: categorySortedData,
+            adId: categoryIndex === 0 ? 5 : categoryIndex === 1 ? 6 : null,
+          });
+        });
+
+        setData(listData);
 
         setDataLoaded(true); // Indicate that data has been loaded
         dispatch(setLoading(false));
@@ -114,9 +187,10 @@ const HomeScreen = () => {
   //   }
   // }, [userInfo, userInfoLoaded]);
 
-  const categoriesData = useSelector(
-    (state) => state.selectedContent.categoriesData
-  ); // Assuming this holds the full categories array
+  // const categoriesData = useSelector(
+  //   (state) => state.selectedContent.categoriesData
+  // );
+  // Assuming this holds the full categories array
   const scrollViewRef = useRef(null);
 
   useEffect(() => {
@@ -155,25 +229,13 @@ const HomeScreen = () => {
     }
   };
 
-  const DATA = [
-    {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-      title: "First Item",
-    },
-    {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-      title: "Second Item",
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d72",
-      title: "Third Item",
-    },
-  ];
-
-  const Item = ({ title }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
+  const Item = ({ item, section, index }) => (
+    <>
+      <Priority key={item._id} article={item} />
+      {section.adId && index === section?.data?.length - 1 && (
+        <AdPlacement id={section.adId} />
+      )}
+    </>
   );
 
   return (
@@ -188,42 +250,66 @@ const HomeScreen = () => {
       {isConnectionError && <NoConnection />}
       {isLoading && <LoadingScreen />}
       {!isLoading && !isConnectionError && (
-        // <FlatList
-        //   style={{ height: "100%" }}
-        //   data={DATA}
-        //   renderItem={({ item }) => <Item title={item.title} />}
-        //   keyExtractor={(item) => item.id}
-        // />
-        <ScrollView
-          bounces={false}
-          overScrollMode="never"
-          ref={scrollViewRef}
-          style={{ height: "100%" }}
-          onMomentumScrollEnd={({ nativeEvent }) => {
-            const isEnd =
-              nativeEvent.layoutMeasurement.height +
-                nativeEvent.contentOffset.y >=
-              nativeEvent.contentSize.height - 20;
-
-            if (isEnd && hasMore) {
-              loadMoreContent();
+        <>
+          <SectionList
+            refreshing={isLoading}
+            style={{ height: "100%" }}
+            sections={data}
+            renderItem={({ item, section, index, seperators }) => {
+              return (
+                <Item
+                  item={item}
+                  section={section}
+                  index={index}
+                  seperators={seperators}
+                />
+              );
+            }}
+            renderSectionHeader={({ section: { title } }) =>
+              title !== "Highlight" && (
+                <View style={styles.categoryContainer}>
+                  <View style={{ backgroundColor: "#fff", paddingBottom: 10 }}>
+                    <View style={styles.categoryName}>
+                      <Text style={styles.category}>{title}</Text>
+                    </View>
+                  </View>
+                </View>
+              )
             }
-          }}
-        >
-          {selectedCategory === "pocetna" && (
-            <>
-              <HighlightNews />
-              <AdPlacement id={2} />
-              {/* <CategoryHighlightNews /> */}
-            </>
-          )}
-          {selectedCategory === "najnovije" && (
-            <Najnovije isPageLoading={isPageLoading} />
-          )}
-          {selectedCategory !== "pocetna" && (
-            <CategoryContent isPageLoading={isPageLoading} />
-          )}
-        </ScrollView>
+            stickySectionHeadersEnabled={false}
+            keyExtractor={(item, index) => `${item.title}-${index}`}
+          />
+          <ScrollView
+            bounces={false}
+            overScrollMode="never"
+            ref={scrollViewRef}
+            style={{ height: "100%" }}
+            onMomentumScrollEnd={({ nativeEvent }) => {
+              const isEnd =
+                nativeEvent.layoutMeasurement.height +
+                  nativeEvent.contentOffset.y >=
+                nativeEvent.contentSize.height - 20;
+
+              if (isEnd && hasMore) {
+                loadMoreContent();
+              }
+            }}
+          >
+            {selectedCategory === "pocetna" && (
+              <>
+                <HighlightNews />
+                <AdPlacement id={2} />
+                <CategoryHighlightNews />
+              </>
+            )}
+            {selectedCategory === "najnovije" && (
+              <Najnovije isPageLoading={isPageLoading} />
+            )}
+            {selectedCategory !== "pocetna" && (
+              <CategoryContent isPageLoading={isPageLoading} />
+            )}
+          </ScrollView>
+        </>
       )}
     </View>
   );
@@ -237,6 +323,20 @@ const styles = StyleSheet.create({
   },
   statusBar: {
     color: "#fff",
+  },
+  categoryName: {
+    backgroundColor: "#1A2F5A",
+    paddingHorizontal: 20,
+    paddingVertical: 4,
+    alignSelf: "flex-start",
+  },
+  category: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  categoryContainer: {
+    marginTop: 20,
   },
 });
 
