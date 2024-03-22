833b2f4dbda4b02deb10695ca66cb54f07f1c0ea
Elmar Ismayilov <bamazy@gmail.com>
2024-03-22 08:22:56 +0800

    uncomment notification token codes

--- screens/Settings.js
+++ screens/Settings.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-// import messaging from "@react-native-firebase/messaging";
+import messaging from "@react-native-firebase/messaging";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API_URL } from "@env";
 
@@ -60,82 +60,79 @@ const Settings = () => {
   const handleNotificationChange = async (value) => {
     setNotificationsEnabled(value); // Update state immediately
 
-    // messaging()
-    //   .getToken()
-    //   .then((token) => {
-    //     sendTokenToServer(token, value);
-    //     handleAsyncToken(token, value);
-    //   });
+    messaging()
+      .getToken()
+      .then((token) => {
+        sendTokenToServer(token, value);
+        handleAsyncToken(token, value);
+      });
   };
 
   return (
-      <View>
-        <StatusBarView backgroundColor="#1A2F5A" />
-        <Header isHome={false} isSettings />
+    <View>
+      <StatusBarView backgroundColor="#1A2F5A" />
+      <Header isHome={false} isSettings />
 
-        <TouchableOpacity
-            style={styles.button}
-
-        >
-          <Image
-              source={require("../assets/notificationIcon.png")}
-              style={styles.backIcon}
-              resizeMode={"contain"}
+      <TouchableOpacity style={styles.button}>
+        <Image
+          source={require("../assets/notificationIcon.png")}
+          style={styles.backIcon}
+          resizeMode={"contain"}
+        />
+        <View style={styles.texts}>
+          <Text style={styles.userInfoText}>Notifikacije</Text>
+          <Switch
+            trackColor={{ false: "#767577", true: "#1a2f5a" }}
+            style={styles.switchStyle}
+            thumbColor={notificationsEnabled ? "#b2c7f5" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={(value) => handleNotificationChange(value)}
+            value={notificationsEnabled}
           />
-          <View style={styles.texts}>
-            <Text style={styles.userInfoText}>Notifikacije</Text>
+        </View>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Upalite Notifikacije</Text>
             <Switch
-                trackColor={{ false: "#767577", true: "#1a2f5a" }}
-                style={styles.switchStyle}
-                thumbColor={notificationsEnabled ? "#b2c7f5" : "#f4f3f4"}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={(value) => handleNotificationChange(value)}
-                value={notificationsEnabled}
+              trackColor={{ false: "#767577", true: "#1a2f5a" }}
+              style={styles.switchStyle}
+              thumbColor={notificationsEnabled ? "#b2c7f5" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={(value) => handleNotificationChange(value)}
+              value={notificationsEnabled}
             />
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Zatvori</Text>
+            </TouchableOpacity>
           </View>
-        </TouchableOpacity>
+        </View>
+      </Modal>
 
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => {
-              setModalVisible(!modalVisible);
-            }}
-        >
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>Upalite Notifikacije</Text>
-              <Switch
-                  trackColor={{ false: "#767577", true: "#1a2f5a" }}
-                  style={styles.switchStyle}
-                  thumbColor={notificationsEnabled ? "#b2c7f5" : "#f4f3f4"}
-                  ios_backgroundColor="#3e3e3e"
-                  onValueChange={(value) => handleNotificationChange(value)}
-                  value={notificationsEnabled}
-              />
-              <TouchableOpacity
-                  style={[styles.button, styles.buttonClose]}
-                  onPress={() => setModalVisible(!modalVisible)}
-              >
-                <Text style={styles.textStyle}>Zatvori</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </Modal>
-
-        <TouchableOpacity style={styles.button}>
-          <Image
-              source={require("../assets/version.png")}
-              style={styles.backIcon}
-              resizeMode={"contain"}
-          />
-          <View style={styles.texts}>
-            <Text style={styles.userInfoText}>Verzija</Text>
-            <Text style={styles.userInfo}>1.0.0</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity style={styles.button}>
+        <Image
+          source={require("../assets/version.png")}
+          style={styles.backIcon}
+          resizeMode={"contain"}
+        />
+        <View style={styles.texts}>
+          <Text style={styles.userInfoText}>Verzija</Text>
+          <Text style={styles.userInfo}>1.0.0</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -148,14 +145,13 @@ const styles = StyleSheet.create({
   switchStyle: {
     transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }], // Scale by 1.5 times the original size
     marginVertical: 10, // Add some vertical margin if needed
-
   },
   button: {
     display: "flex",
     alignItems: "center",
     flexDirection: "row",
     backgroundColor: "#ffffff",
-    height:90,
+    height: 90,
     paddingVertical: 14,
     paddingHorizontal: 20,
     borderWidth: 1,
@@ -165,7 +161,7 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     flex: 1, // Added flex to make the texts take the remaining space
     flexDirection: "row", // Align texts and switch horizontally
-    alignItems:"center",
+    alignItems: "center",
     justifyContent: "space-between", // Align texts and switch horizontally
   },
   backIcon: {
